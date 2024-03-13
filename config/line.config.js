c66cacc8c732a7329bb4557edd12e5867993b013
andy.lai@framy.co <andy.lai@framy.co>
2024-03-13 03:12:50 +0000

    Commit #2: Set up necessary configurations for LINE Messaging API and MongoDB.

---
+++ config/line.config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  channelAccessToken: "3aDiL2bsqyOmpiX4m+xebdMZ6J5rRtfAsaZoWioYSn6XqH0CuM5OXSbmoPLTfSt7RsIkbVgkpkz+5c+DyOH7fy9WUuJ/Ei3iA+31uQShcEfuAmpzcXR+n1NPW+jEOufF5GdmbktEWRFlmH30tcamjQdB04t89/1O/w1cDnyilFU="
+};
+
---
+++ config/mongo.config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  mongoURI: "mongodb://mongodb-container:27017/line_bot"
+};
+
